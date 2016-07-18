0735f08682eada894fa58f840624b2c470d23252
Tracless <bastianeder@hotmail.com>
2016-07-18 15:44:13 +0200

    update
    added jquery

---
+++ js/response.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $(".pseudo").hover(function(){
+        $(this).addClass("pseudo2");
+    },
+    function(){
+        $(this).removeClass("pseudo2");
+        
+    }
+  );
+
+    
+
+});
+       
+
+
+
